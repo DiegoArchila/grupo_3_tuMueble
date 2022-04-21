2a5ebd80f2a428d593934d175bf05fc4fe9a81f3
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-04-21 01:06:12 -0500

    [NEW] Api de consulta de productos

---
+++ src/routes/apis/ProductsRouts.js
@@ -0,0 +1,21 @@
+/* Imports */
+const express = require("express");
+const router = express.Router();
+
+const productsController = require("../../controllers/apis/ProductsController.js");
+
+const urlProducts = "/api/products";
+
+//All products
+router.get(`${urlProducts}`, productsController.findAll);
+
+//Product detail
+router.get(`${urlProducts}/detail/:id`, productsController.findProduct);
+
+//Products by category
+router.get(
+  `${urlProducts}/category/:categoryId`,
+  productsController.findProductsByCategory
+);
+
+module.exports = router;
